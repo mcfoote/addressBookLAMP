6bf391a5a471fb013e3595c7710cdd7d16e95221
Mitchell Foote <mcfoote321@gmail.com>
2023-04-02 20:35:26 -0400

    work on utilities

--- includes/utility.php
+++ includes/utility.php
@@ -3,5 +3,11 @@ function listAllContacts($db) {
     $sql = 'SELECT * FROM contact Join city
             ON contact.cityid=city.id
             ORDER BY contact.id DESC';
+    $stmt=$db->prepare($sql);
+    $stmt->execute();
+    $result=$stmt->fetchAll();
+    return $result;
 }
+
+
 ?>
